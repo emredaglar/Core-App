890bec13e39100d660475189992b0b56409d3bc9
emredaglar <emre.daglar1@gmail.com>
2024-11-04 20:07:52 +0300

    Added Context

---
+++ DataAccessLayer/Context.cs
@@ -0,0 +1,27 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+	public class Context:DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=DD\\SQLEXPRESS01;database=CoreAppDb;integrated security=true");
+		}
+		public DbSet<About> Abouts { get; set; }
+		public DbSet<Contact> Contacts { get; set; }
+		public DbSet<Experience> Experiences { get; set; }
+		public DbSet<Feature> Features { get; set; }
+		public DbSet<Message> Messages { get; set; }
+		public DbSet<Portfolio> Portfolios { get; set; }
+		public DbSet<Service> Skills { get; set; }
+		public DbSet<SocailMedia> SocailMedias { get; set; }
+		public DbSet<Testimonial> Testimonials { get; set; }
+	}
+}
