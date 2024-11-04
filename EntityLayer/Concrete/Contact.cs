bf9e2acab95883a2020a4b40ba3db78f0fe8d83c
emredaglar <emre.daglar1@gmail.com>
2024-11-04 19:49:48 +0300

    Added Entities and EFCore Package

---
+++ EntityLayer/Concrete/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+	public class Contact
+	{
+		public int ContactID { get; set; }
+		public int Title { get; set; }
+		public int Description { get; set; }
+		public int Mail { get; set; }
+		public int Phone { get; set; }
+	}
+}
---
+++ EntityLayer/Concrete/Testimonial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+	public class Testimonial
+	{
+        public int TestimonialID { get; set; }
+        public int ClientName { get; set; }
+        public int Company { get; set; }
+        public int Comment { get; set; }
+        public int ImageUrl { get; set; }
+    }
+}
