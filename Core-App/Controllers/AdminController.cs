8ee592b88f769f30a4b6dc7967f7628ebeb03ada
emredaglar <emre.daglar1@gmail.com>
2024-11-07 01:04:39 +0300

    Added  AdminController and Partialview

---
+++ Core-App/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_App.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialHeader() // eğitimde NavigationPartial adını veriyor
+        {
+            return PartialView();
+        }
+    }
+}
