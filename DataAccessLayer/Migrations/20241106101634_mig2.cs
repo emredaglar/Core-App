d5c26432e0703b663826b28f5370c96acd24e873
emredaglar <emre.daglar1@gmail.com>
2024-11-06 15:27:23 +0300

    added Portfolio

---
+++ DataAccessLayer/Migrations/20241106101634_mig2.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProjectUrl",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProjectUrl",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Portfolios");
+        }
+    }
+}
