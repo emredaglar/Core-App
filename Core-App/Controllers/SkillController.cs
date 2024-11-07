1c8a7f621936b816eb897dbe24a21ec40e4b0997
emredaglar <emre.daglar1@gmail.com>
2024-11-07 12:27:58 +0300

    added SkillController and NewTemplate

---
+++ Core-App/Controllers/SkillController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFreamwork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_App.Controllers
+{
+    public class SkillController : Controller
+    {
+        SkillManager skillManager=new SkillManager(new EfSkillDal());
+        public IActionResult Index()
+        {
+            var value=skillManager.TGetList();
+            return View(value);
+        }
+    }
+}
