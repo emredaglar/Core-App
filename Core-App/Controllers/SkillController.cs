24c9495c067a15605f740e8c1ed837febc59e2f1
emredaglar <emre.daglar1@gmail.com>
2024-11-07 17:55:20 +0300

    added SkillController

--- Core-App/Controllers/SkillController.cs
+++ Core-App/Controllers/SkillController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFreamwork;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_App.Controllers
@@ -9,8 +10,40 @@ namespace Core_App.Controllers
         SkillManager skillManager=new SkillManager(new EfSkillDal());
         public IActionResult Index()
         {
+            ViewBag.Url1 = "Yetenekler";
+            ViewBag.Url2 = "Skill";
             var value=skillManager.TGetList();
             return View(value);
         }
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSkill(Skill skill)
+        {
+            skillManager.TAdd(skill);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditSkill(int id)
+        {
+            var values=skillManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditSkill(Skill skill)
+        {
+            skillManager.TUpdate(skill);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteSkill(int id)
+        {
+            var values= skillManager.TGetByID(id);
+            skillManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
     }
 }
