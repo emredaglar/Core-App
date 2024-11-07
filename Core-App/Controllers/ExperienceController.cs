7bd511b69fd6508adda191da06449dfb72f6c572
emredaglar <emre.daglar1@gmail.com>
2024-11-07 19:36:32 +0300

    added Admin Experience

---
+++ Core-App/Controllers/ExperienceController.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFreamwork;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_App.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+
+        public IActionResult Index()
+        {
+            var value= experienceManager.TGetList();
+            return View(value);
+        }
+        [HttpGet]
+        public IActionResult AddExperience()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddExperience(Experience experience)
+        {
+            experienceManager.TAdd(experience);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditExperience(int id)
+        {
+            var value=experienceManager.TGetByID(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult EditExperience(Experience experience)
+        {
+            
+            experienceManager.TUpdate(experience);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteExperience(int id)
+        {
+            var value = experienceManager.TGetByID(id);
+            experienceManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+    }
+}
