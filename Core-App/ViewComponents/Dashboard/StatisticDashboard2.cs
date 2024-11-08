d0d495756b52faab9cff48a8f35f4c72f8c36444
emredaglar <emre.daglar1@gmail.com>
2024-11-09 01:09:16 +0300

    added SecondStatistic

---
+++ Core-App/ViewComponents/Dashboard/StatisticDashboard2.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_App.ViewComponents.Dashboard
+{
+    public class StatisticDashboard2 : ViewComponent
+    {
+        Context context=new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = context.Portfolios.Count();
+            ViewBag.v2 = context.Messages.Count();
+            ViewBag.v3 = context.Services.Count();
+            return View();
+        }
+    }
+}
