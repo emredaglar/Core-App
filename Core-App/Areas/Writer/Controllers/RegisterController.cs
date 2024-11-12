83206487326c9697e030447e9b26ea37d763004c
emredaglar <emre.daglar1@gmail.com>
2024-11-12 23:53:45 +0300

    Added IdentityPackages

---
+++ Core-App/Areas/Writer/Controllers/RegisterController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_App.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string t)
+        {
+            return View();
+        }
+    }
+}
