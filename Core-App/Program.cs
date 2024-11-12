6a1c84c58824680fde78079ada703c38956734f1
emredaglar <emre.daglar1@gmail.com>
2024-11-12 23:38:15 +0300

    added WriterTemplate

--- Core-App/Program.cs
+++ Core-App/Program.cs
@@ -14,12 +14,21 @@ if (!app.Environment.IsDevelopment())
 	app.UseExceptionHandler("/Home/Error");
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
+
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Default}/{action=Index}/{id?}"
+    );
+});
 app.UseRouting();
 
 app.UseAuthorization();
@@ -27,5 +36,6 @@ app.UseAuthorization();
 app.MapControllerRoute(
 	name: "Default",
 	pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
