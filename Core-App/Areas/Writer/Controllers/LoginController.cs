66393f19c1260f044e219bc9d3d81a1ba23724aa
emredaglar <emre.daglar1@gmail.com>
2024-11-13 17:42:22 +0300

    added Login

---
+++ Core-App/Areas/Writer/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Core_App.Areas.Writer.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_App.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<WriterUser> _signInManager;
+
+        public LoginController(SignInManager<WriterUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserLoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, true, true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Default");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Hatalı Kullanıcı Adı veya Şifre");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Core-App/Areas/Writer/Models/UserLoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core_App.Areas.Writer.Models
+{
+    public class UserLoginViewModel
+    {
+        [Display(Name ="Kullanıcı Adı")]
+        public string UserName { get; set; }
+        [Display(Name = "Şifre")]
+        public string Password { get; set; }
+    }
+}
