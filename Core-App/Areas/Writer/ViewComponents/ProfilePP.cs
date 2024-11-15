15acd9cfaf6624954d0f08989ed799643bfd1fbf
emredaglar <emre.daglar1@gmail.com>
2024-11-15 11:34:28 +0300

    added DinamicProfilePicture

---
+++ Core-App/Areas/Writer/ViewComponents/ProfilePP.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer.Migrations;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_App.Areas.Writer.ViewComponents
+{
+	public class ProfilePP:ViewComponent
+	{
+		private readonly UserManager<WriterUser> _userManager;
+
+		public ProfilePP(UserManager<WriterUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var values = await _userManager.FindByNameAsync(User.Identity.Name);
+			ViewBag.v = values.ImageUrl;
+			return View();
+		}
+	}
+}
