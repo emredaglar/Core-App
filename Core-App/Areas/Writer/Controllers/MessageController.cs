a36b505cb835fcaee401f1617dc56fd0540d7094
emredaglar <emre.daglar1@gmail.com>
2024-11-15 11:04:00 +0300

    added Notification

---
+++ Core-App/Areas/Writer/Controllers/MessageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_App.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class MessageController : Controller
+    {
+        public IActionResult MessageIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ Core-App/Areas/Writer/ViewComponents/Notification.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFreamwork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_App.Areas.Writer.ViewComponents
+{
+	public class Notification : ViewComponent
+	{ 
+		AnnouncementManager _announcementManager=new AnnouncementManager(new EfAnnouncementDal());
+
+		public IViewComponentResult Invoke()
+		{
+			var values = _announcementManager.TGetList().OrderByDescending(x=>x.ID).Take(3).ToList();
+			return View(values);
+		}
+	}
+}
