8a749720777664ee40fe391f2ceee10e4803d31c
emredaglar <emre.daglar1@gmail.com>
2024-11-16 17:31:04 +0300

    added DefaultSocailMediaList

---
+++ Core-App/ViewComponents/SocialMedia/SocailMediaList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFreamwork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_App.ViewComponents.SocialMedia
+{
+    public class SocailMediaList:ViewComponent
+    {
+        SocailMediaManager _socailMediaManager=new SocailMediaManager(new EfSocailMediaDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _socailMediaManager.TGetList();
+            return View(values);
+        }
+    }
+}
