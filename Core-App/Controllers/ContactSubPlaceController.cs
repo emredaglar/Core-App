ac0c6d451dc3d33906ea198f89e1316364608065
emredaglar <emre.daglar1@gmail.com>
2024-11-16 16:47:54 +0300

    added ContactSubPlace

---
+++ Core-App/Controllers/ContactSubPlaceController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFreamwork;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_App.Controllers
+{
+    public class ContactSubPlaceController : Controller
+    {
+        ContactManager _contactManager = new ContactManager(new EfContactDal());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var values=_contactManager.TGetByID(1);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            _contactManager.TUpdate(contact);
+            return RedirectToAction("Index");
+        }
+    }
+}
