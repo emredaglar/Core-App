8121d9a2c3ed491e20f310d72d060202e5bba8e5
emredaglar <emre.daglar1@gmail.com>
2024-11-16 17:07:06 +0300

    added SocailMedia

---
+++ Core-App/Controllers/SocailMediaController.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFreamwork;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_App.Controllers
+{
+    public class SocailMediaController : Controller
+    {
+        SocailMediaManager socialMediaManager = new SocailMediaManager(new EfSocailMediaDal());
+        public IActionResult Index()
+        {
+            var values = socialMediaManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddSocialMedia()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddSocialMedia(SocailMedia p)
+        {
+            p.Status = true;
+            socialMediaManager.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            var values = socialMediaManager.TGetByID(id);
+            socialMediaManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditSocialMedia(int id)
+        {
+            var values = socialMediaManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditSocialMedia(SocailMedia p)
+        {
+            socialMediaManager.TUpdate(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
