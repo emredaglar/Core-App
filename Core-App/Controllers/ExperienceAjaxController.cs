8c1485a53bf4425e4981c866a929182a5075d36c
emredaglar <emre.daglar1@gmail.com>
2024-11-19 22:06:17 +0300

    added AJAX

---
+++ Core-App/Controllers/ExperienceAjaxController.cs
@@ -0,0 +1,53 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFreamwork;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Core_App.Controllers
+{
+    public class ExperienceAjaxController : Controller
+    {
+        ExperienceManager experienceManager = new ExperienceManager(new EfExperienceDal());
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        public IActionResult ListExperience()
+        {
+            var values = JsonConvert.SerializeObject(experienceManager.TGetList());
+            return Json(values);
+        }
+        [HttpPost]
+        public IActionResult AddExperience(Experience experience)
+        {
+            experienceManager.TAdd(experience);
+            var values = JsonConvert.SerializeObject(experience);
+            return Json(values);
+        }
+        public IActionResult GetById(int id)
+        {
+            var v=experienceManager.TGetByID(id);
+            var values=JsonConvert.SerializeObject(v);
+            return Json(values);
+
+        }
+        public IActionResult DeleteExperience(int id)
+        {
+            var v = experienceManager.TGetByID(id);
+            experienceManager.TDelete(v);
+            return NoContent();
+
+        }
+
+
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience p)
+        {
+            experienceManager.TUpdate(p);
+            var values = JsonConvert.SerializeObject(p);
+            return Json(values);
+        }
+    }
+}
---
+++ Core-App/Controllers/WriterUserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_App.Controllers
+{
+    public class WriterUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
