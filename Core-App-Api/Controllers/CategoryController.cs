7b0360adda1ead1a88d29e8210b6b6ac30d4c327
emredaglar <emre.daglar1@gmail.com>
2024-11-20 10:45:23 +0300

    added Api-AddDb

---
+++ Core-App-Api/Controllers/CategoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_App_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+    }
+}
---
+++ Core-App-Api/DAL/ApiContext/Context.cs
@@ -0,0 +1,14 @@
+﻿using Core_App_Api.DAL.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core_App_Api.DAL.ApiContext
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DD\\SQLEXPRESS01;database=CoreAppApiDb;integrated security=true;TrustServerCertificate=True;");
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
