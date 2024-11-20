0c275e7a1b7a5c22081897cd49cfb1dd00a949d7
emredaglar <emre.daglar1@gmail.com>
2024-11-20 11:04:50 +0300

    added Api

--- Core-App-Api/Controllers/CategoryController.cs
+++ Core-App-Api/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Core_App_Api.DAL.ApiContext;
+using Core_App_Api.DAL.Entity;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_App_Api.Controllers
@@ -7,5 +9,70 @@ namespace Core_App_Api.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            using var context = new Context();
+            return Ok(context.Categories.ToList());
+        }
+        [HttpGet("{id}")]
+        public IActionResult CategoryGetId(int id)
+        {
+            using var context = new Context();
+            var value = context.Categories.Find(id);
+            if (value == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(value);
+            }
+        }
+        [HttpPost]
+        public IActionResult CategoryAdd(Category category)
+        {
+            using var context = new Context();
+            context.Add(category);
+            context.SaveChanges();
+            return Created("", category);
+        }
+        [HttpDelete]
+        public IActionResult CategoryDelete(int id)
+        {
+            using var context = new Context();
+
+            var value = context.Categories.Find(id);
+            if (value == null)
+            {
+                return NotFound();
+
+            }
+            else
+            {
+                context.Remove(value);
+                context.SaveChanges();
+                return NoContent();
+            }
+        }
+        [HttpPut]
+        public IActionResult CategoryUpdate(Category category)
+        {
+            using var context = new Context();
+            var value = context.Find<Category>(category.CategoryID);
+            if (value == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                value.CategoryName = category.CategoryName;
+                context.Update(value);
+                context.SaveChanges();
+                return NoContent();
+            }
+        }
+
+
     }
 }
